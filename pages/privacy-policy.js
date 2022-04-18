f62d9dc65b81c509cef15f739d6976032bbf8e12
Aymen Fezai <fezaiaymen1994@gmail.com>
2022-04-18 15:26:04 +0100

    ADD: PP and TaC

---
+++ pages/privacy-policy.js
@@ -0,0 +1,295 @@
+import Layout from "../component/layout/Layout";
+
+const PrivacyPolicy = () => {
+	return (
+		<>
+			<Layout isStaticNav>
+				<section>
+					<div className='container'>
+						<h1>Privacy Policy</h1>
+						<p>Last updated December 21, 2021</p>
+
+						<p>
+							Thank you for choosing to be part of our community at Screen Flex (&quot;Company,&quot; &quot;we,&quot;
+							&quot;us,&quot; or &quot;our&quot;). We are committed to protecting your personal information and your
+							right to privacy. If you have any questions or concerns about this privacy notice or our practices with
+							regard to your personal information, please contact us at contact@screenflex.pro.
+						</p>
+
+						<p>This privacy notice describes how we might use your information if you:</p>
+						<ul>
+							<li>Visit our website at screenflex.pro</li>
+							<li>
+								Engage with us in other related ways — including any sales, marketing, or events. In this privacy
+								notice, if we mention:
+							</li>
+							<li>&quot;Website,&quot; we are referring to any of our websites that is linked to this policy</li>
+							<li>
+								&quot;Services,&quot; we are referring to our Website, and other related services, including any sales,
+								marketing, or events
+							</li>
+						</ul>
+
+						<p>
+							The purpose of this privacy notice is to make it as clear for you as possible what information we collect,
+							how we use it, and what rights you have in relation to it. If there are any terms in this privacy notice
+							that you do not agree with, please discontinue using our Services immediately. If you want to understand
+							what we do with the collected information, please continue carefully reading this policy notice as it will
+							help you follow the procedures in details
+						</p>
+
+						<h2>1. WHAT INFORMATION DO WE COLLECT?</h2>
+						<p>
+							We collect the personal information that you voluntarily provide us with when you express an interest in
+							obtaining information about us, our products and services, when you participate in activities on the
+							Website, or when you contact us. The personal information that we collect depends on the context of your
+							interactions with us and the Website, the choices you make, and the products and features you use. The
+							personal information we collect may include the following:
+						</p>
+						<p>
+							Personal Information Provided by You. We collect email addresses; names; phone numbers; billing addresses;
+							debit/credit card numbers; and other similar information. All personal information that you provide us
+							with must be true, complete, and accurate. You must notify us in case of any changes in your personal
+							information.
+						</p>
+
+						<h2>2. HOW DO WE USE YOUR INFORMATION?</h2>
+						<p>
+							We process your information for purposes based on legitimate business interests, the fulfillment of our
+							contract with you, compliance with our legal obligations, and/or your consent. We use personal information
+							collected via our Website for a variety of business purposes described below. We process your personal
+							information for these purposes in reliance with our legitimate business interests, in order to enter into
+							or perform a contract with you, with your consent, and/or for compliance with our legal obligations. The
+							specific processing grounds we rely on are listed below.
+						</p>
+						<p>We use the information we collect or receive to:</p>
+						<ul>
+							<li>
+								Fulfill and manage your orders. We may use your information to fulfill and manage your orders, payments,
+								returns, and exchanges made through the Website.
+							</li>
+							<li>
+								Administer prize draws and competitions. We may use your information to administer prize draws and
+								competitions when you choose to participate in our competitions.
+							</li>
+							<li>
+								To deliver and facilitate the delivery of services to the user. We may use your information to provide
+								you with the requested service.
+							</li>
+							<li>
+								To respond to the inquiries/offer support of users. We may use your information to respond to your
+								inquiries and solve any potential issues you might have with the use of our Services.
+							</li>
+						</ul>
+
+						<h2>3. WILL YOUR INFORMATION BE SHARED WITH ANYONE?</h2>
+						<p>
+							We only share information with your consent, to comply with laws, to provide you with services, to protect
+							your rights or to fulfill business obligations.
+						</p>
+						<p>We may process or share the data that we hold based on the following legal basis: </p>
+						<ul>
+							<li>
+								Consent: We may process your data if you gave us specific consent to use your personal information for a
+								specific purpose.
+							</li>
+							<li>
+								Legitimate Interests: We may process your data when it is reasonably necessary to achieve our legitimate
+								business interests.
+							</li>
+							<li>
+								Performance of a Contract: when we contract with you, we may process your personal information to
+								fulfill the terms of our contract.
+							</li>
+							<li>
+								Legal Obligations: We may disclose your information when we are legally required to do so in order to
+								comply with applicable law, governmental requests, a judicial proceeding, court order, or legal
+								processes, such as in response to a court order or a subpoena (including in response to public
+								authorities to meet national security or law enforcement requirements).
+							</li>
+							<li>
+								Vital Interests: We may disclose your information when we believe it is necessary to investigate,
+								prevent, or take action regarding potential violations of our policies, suspected fraud, situations
+								involving potential threats to the safety of any person and illegal activities, or as evidence in
+								litigation in which we are involved.
+							</li>
+						</ul>
+
+						<p>
+							To make it more obvious for you, we may need to process your data or share your personal information in
+							the following situations:
+						</p>
+						<ul>
+							<li>
+								Business Transfers. We may share or transfer your information in connection with, or during negotiations
+								of, any merger, sale of company assets, financing, or acquisition of all or a portion of our business to
+								another company.
+							</li>
+							<li>
+								Affiliates. We may share your information with our affiliates, in this case we will require those
+								affiliates to honor this privacy notice. Affiliates include our parent company and any subsidiaries,
+								joint venture partners, or other companies that we control or that are under common control with us.
+							</li>
+						</ul>
+
+						<h2>4. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</h2>
+						<p>
+							We may use cookies and other tracking technologies (like web beacons and pixels) to collect and store your
+							information. Specific information about how we use such technologies and how you can refuse certain
+							cookies is set out in our Cookie Notice.
+						</p>
+
+						<h2>5. HOW LONG DO WE KEEP YOUR INFORMATION?</h2>
+						<p>
+							We keep your information for as long as necessary to fulfill the purposes outlined in this privacy notice
+							unless required by law.
+						</p>
+						<p>
+							We will only keep your personal information for as long as it is necessary for the purposes set out in
+							this privacy notice unless a longer retention period is required or permitted by law (such as tax,
+							accounting, or other legal requirements). No purpose in this notice will require us to keep your personal
+							information for longer than 1 year.
+						</p>
+						<p>
+							When we have no ongoing legitimate business need to process your personal information, we will either
+							delete or anonymize such information or, if this is not possible (for example, because your personal
+							information has been stored in backup archives), then, we will securely store your personal information
+							and isolate it from any further processing until deletion is possible.
+						</p>
+
+						<h2>6. HOW DO WE KEEP YOUR INFORMATION SAFE?</h2>
+						<p>
+							We aim to protect your personal information through a system of organizational and technical security
+							measures.
+						</p>
+						<p>
+							We have implemented appropriate technical and organizational security measures designed to protect the
+							security of any personal information we process. However, despite our safeguards and efforts to secure
+							your information, no electronic transmission over the Internet or information storage technology can be
+							guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other
+							unauthorized third parties will not be able to defeat our security, and improperly collect, access, steal,
+							or modify your information. Although we will do our best to protect your personal information, the
+							transmission of personal information to and from our Website is at your own risk. You should access the
+							Website only within a secure environment.
+						</p>
+
+						<h2>7. DO WE COLLECT INFORMATION FROM MINORS?</h2>
+						<p>
+							We do not knowingly collect data from or market to children under 18 years of age. We do not knowingly
+							solicit data from or market to children under 18 years of age. By using the Website, you represent that
+							you are at least 18 or that you are the parent or guardian of such a minor and show your consent to
+							his/her use of the Website. If we learn that the personal information has been collected from users less
+							than 18 years of age, we will deactivate the account and take reasonable measures to promptly delete such
+							data from our records. If you know about any data that we may have collected from children under age 18,
+							please contact us at contact@screenflex.pro.
+						</p>
+
+						<h2>8. WHAT ARE YOUR PRIVACY RIGHTS?</h2>
+						<p>
+							You may review, change, or terminate your account at any time. If you are a resident in the EEA or UK and
+							you believe we are unlawfully processing your personal information, you also have the right to complain to
+							your local data protection supervisory authority.
+						</p>
+
+						<p>
+							Cookies and similar technologies: Most Web browsers are set to accept cookies by default. If you prefer,
+							you can usually choose to set your browser to remove and to reject cookies. If you choose to remove
+							cookies or reject them, this could affect certain features or services of our Website. To opt-out of
+							interest-based advertising by advertisers on our Website contact us at contact@screenflex.pro.
+						</p>
+
+						<h2>9. CONTROLS FOR DO-NOT-TRACK FEATURES</h2>
+						<p>
+							Most web browsers and some mobile operating systems and mobile applications include a Do-Not-Track
+							(&quot;DNT&quot;) feature or setting you can activate to signal your privacy preference not to have data
+							about your online browsing activities monitored and collected. At this stage, no uniform technology
+							standard for recognizing and implementing DNT signals has been finalized. As such, we do not currently
+							respond to DNT browser signals or any other mechanism that automatically communicates your choice not to
+							be tracked online. If a standard for online tracking is adopted, we must follow it in the future. we will
+							inform you about that practice in a revised version of this privacy notice.
+						</p>
+
+						<h2>10. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?</h2>
+						<p>
+							Yes, if you are a resident of California, you are granted specific rights regarding access to your
+							personal information.
+						</p>
+
+						<p>
+							California Civil Code Section 1798.83, also known as the &quot;Shine The Light&quot; law, permits our
+							users who are California residents to request and obtain from us, once a year and free of charge,
+							information about categories of personal information (if any) we disclosed to third parties for direct
+							marketing purposes and the names and addresses of all third parties with which we shared personal
+							information in the immediately preceding calendar year. If you are a California resident and would like to
+							make such a request, please submit your request by writing to us using the contact information provided
+							below.
+						</p>
+
+						<p>
+							If you are under 18 years of age, reside in California, and have a registered account with the Website,
+							you have the right to request the removal of unwanted data that you publicly post on the Website. To
+							request the removal of such data, please contact us using the contact information provided below, and
+							include the email address associated with your account and a statement that you reside in California. We
+							will make sure the data is not publicly displayed on the Website, but please be aware that the data may
+							not be completely or comprehensively removed from all our systems (e.g. backups, etc.).
+						</p>
+
+						<h2>11. DO WE MAKE UPDATES TO THIS NOTICE?</h2>
+						<p>
+							Yes, we will update this notice as necessary to stay compliant with relevant laws. We may update this
+							privacy notice from time to time. The updated version will be indicated by an updated &quot;Revised&quot;
+							date and the updated version will be effective as soon as it is accessible. If we make material changes to
+							this privacy notice, we may notify you either by prominently posting a notice of such changes or by
+							directly sending you a notification. We encourage you to review this privacy notice frequently to be
+							informed of how we are protecting your information.
+						</p>
+
+						<h2>12. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</h2>
+						<p>
+							If you have questions or comments about this notice, you may email us at contact@screenflex.pro or by post
+							to: <br />
+							Screen Flex Technopark B21 <br />
+							Pepiniere des Startups El Ghazala, <br />
+							Ariana 2088 <br />
+							Tunisia
+						</p>
+
+						<h2>13. HOW CAN YOU REVIEW, UPDATE, OR DELETE THE DATA WE COLLECT FROM YOU? </h2>
+						<p>
+							Based on the applicable laws of your country, you may have the right to request access to the personal
+							information we collect from you, change that information, or delete it in some circumstances. In order to
+							request reviewing, updating, or deleting your personal information, please contact us.
+						</p>
+					</div>
+				</section>
+			</Layout>
+
+			<style jsx>{`
+				section {
+					padding: 60px 0px;
+				}
+
+				p {
+					margin-top: 20px;
+					font-size: 16px;
+				}
+
+				ul {
+					margin-left: 60px;
+				}
+
+				li {
+					font-size: 16px;
+				}
+
+				h2 {
+					font-size: 20px;
+					margin-top: 30px;
+					margin-bottom: 20px;
+				}
+			`}</style>
+		</>
+	);
+};
+
+export default PrivacyPolicy;
